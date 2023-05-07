cefe89e406a0066287b32ff9130c8903e084f8e6
sanyi <major.sanyi@outlook.com>
2023-05-07 14:09:46 +0200

    Add project files.

---
+++ SSHKeyGen/Program.cs
@@ -0,0 +1,46 @@
+﻿using SSHKeyGenerator= SshKeyGenerator.SshKeyGenerator;
+
+namespace SSHKeyGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var keygen = new MultipleKeyGen(File.ReadAllLines("militaryAlphabet.txt"));
+             keygen.ExportKeys();
+            Console.WriteLine("GG");
+        }
+    }
+
+    class MultipleKeyGen
+    {
+        List<string> keyNames;
+        Dictionary<string, SSHKeyGenerator> keyGens;
+        const int KeySize = 4096;
+        static string outputDir = "GenKeys";
+
+        public MultipleKeyGen(IEnumerable<string> keyNames)
+        {
+            this.keyNames = keyNames.ToList();
+            keyGens = new Dictionary<string, SSHKeyGenerator>();
+
+            //Fuck Random lol
+            this.keyNames.ForEach(x => { Thread.Sleep(21);  keyGens.Add(x, new SSHKeyGenerator(KeySize)); });
+        }
+
+        public void ExportKeys()
+        {
+            if (Directory.Exists(outputDir))
+            {
+                Directory.Delete(outputDir, true);
+            }
+            Directory.CreateDirectory(outputDir);
+            foreach (var item in keyGens)
+            {
+                Directory.CreateDirectory($"{outputDir}/{item.Key}");
+                File.WriteAllText($"{outputDir}/{item.Key}/public", item.Value.ToPublicKey());
+                File.WriteAllText($"{outputDir}/{item.Key}/private", item.Value.ToPrivateKey());
+            }
+        }
+    }
+}
